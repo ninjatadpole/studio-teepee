af84aacce93c43b7e1ee4f0025c49cb4337d9f0f
Andy Quin <andy@sharpfrog.com>
2017-09-29 10:10:00 +0100

    footer styling

--- teepee/src/components/footer.jsx
+++ teepee/src/components/footer.jsx
@@ -1,40 +1,41 @@
 import React from 'react';
 
-
+import '../styles/footer.css';
 
 function Footer (props) {
   return (
-    <footer className="row container-fluid">
-      <ul className="col-xs-6 contact vcard">
+    <footer className="row container-fluid center-xs start-sm">
+      <ul className="col-xs-8 col-sm-6 contact vcard">
         <li className="adr address">
-          A:
-          <span className="street-address">4 Willow Walk</span>,
-          <span className="locality">Ely</span>,
+          <abbr title="address">A:</abbr>
+          <span className="street-address">4 Willow Walk</span>
+          <span className="locality">Ely</span>
           <span className="postal-code">CB7 4AT</span>
         </li>
-        <li className="tel">
-          <a href="tel:01353611721">01353 611721</a>
-          /
-          <a href="tel:07534323431">07534 323431</a>
+        <li className="tel phone">
+          <abbr title="telephone">T:</abbr>
+          <a href="tel:01353611721" title="land line">01353 611721</a>
+          <a href="tel:07534323431" title="mobile">07534 323431</a>
         </li>
         <li className="email">
+          <abbr title="email">E:</abbr>
           <a href="mailto:office@studioteepee.co.uk">office@studioteepee.co.uk</a>
         </li>
       </ul>
 
-      <ul className="col-xs-6 social">
+      <ul className="col-xs-8 col-sm-6-offset-0 col-sm-6 end-sm social">
         <li className="linkedin">
-          <a href="https://www.linkedin.com/company/18096317/">
+          <a href="https://www.linkedin.com/company/18096317/" className="icon-logo-linkedin">
             <span className="title">LinkedIn</span>
           </a>
         </li>
         <li className="facebook">
-          <a href="https://www.facebook.com/studioteepee.hi">
+          <a href="https://www.facebook.com/studioteepee.hi" className="icon-logo-facebook">
             <span className="title">Facebook</span>
           </a>
         </li>
         <li className="twitter">
-          <a href="https://twitter.com/studioteepee_hi">
+          <a href="https://twitter.com/studioteepee_hi" className="icon-logo-twitter">
             <span className="title">Twitter</span>
           </a>
         </li>
