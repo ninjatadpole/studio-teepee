34922769ca2a751da59d1bbb17edb0390d0959b0
Andy Quin <andy@sharpfrog.com>
2017-10-03 23:09:04 +0100

    Add default route to show home page

--- teepee/src/App.js
+++ teepee/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Footer from './components/footer.jsx'
 import Header from './components/header.jsx'
@@ -11,25 +11,19 @@ const HomeOwner = () => <main><h2>Home Owner</h2></main>
 const Developer = () => <main><h2>Developer</h2></main>
 const Experience = () => <main><h2>Experience</h2></main>
 
-// wrap <Route> and use this everywhere instead, then when
-// sub routes are added to any route it'll work
-const RouteWithSubRoutes = (route) => (
-  <Route path={route.path} render={props => (
-    // pass the sub-routes down to keep nesting
-    <route.component {...props} routes={route.routes}/>
-  )}/>
-)
-
 const App = () => (
   <Router>
     <div>
       <div className="teepee wrapper container-fluid">
         <Header />
 
-        <Route exact path="/" component={ Home }/>
-        <Route path="/developer" component={ Developer }/>
-        <Route path="/experience" component={ Experience }/>
-        <Route path="/homeowner" component={ HomeOwner }/>
+        <Switch>
+          <Route exact path="/" component={ Home }/>
+          <Route path="/developer" component={ Developer }/>
+          <Route path="/experience" component={ Experience }/>
+          <Route path="/homeowner" component={ HomeOwner }/>
+          <Route component={ Home }/>
+        </Switch>
 
         <Footer />
       </div>
