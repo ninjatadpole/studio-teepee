b79c555dc8e0d8fa07afbdc6a030579717d385ef
Andy Quin <andy@sharpfrog.com>
2017-10-05 00:17:02 +0100

    Use DOM text of home page big blocks instead of pseudo element. Scale font size to a limited amount

--- teepee/src/pages/home.jsx
+++ teepee/src/pages/home.jsx
@@ -5,8 +5,8 @@ const Home = function(props) {
   return (
     <main className="page-home">
       <div className="wide">
-        <Link to="/homeowner" className="homeowner"><span>Homeowner</span></Link>
-        <Link to="/homeowner" className="developer"><span>Developer</span></Link>
+        <Link to="/homeowner" className="homeowner"><span>homeowner</span></Link>
+        <Link to="/developer" className="developer"><span>developer</span></Link>
       </div>
     </main>
   );
