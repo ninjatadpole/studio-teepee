2f87b3db1da0e6ced79db0cc68e2fbc548c5cd0c
Andy Quin <andy@sharpfrog.com>
2017-11-06 22:35:28 +0000

    initial styling of contact page & form

--- teepee/src/pages/contact.jsx
+++ teepee/src/pages/contact.jsx
@@ -1,79 +1,59 @@
 import React from 'react';
 
+import ContactForm from '../components/contact-form';
+import SocialLinks from '../components/social-links';
+
 const Contact = function(props) {
   return (
     <main className="page-contact">
-      <div className="introduction">
+      <h1>Get in touch</h1>
+      <div className="content">
+        <div className="contact-types">
+          <dl>
+            <dt>
+              Email
+            </dt>
+            <dd>
+              <a href="mailto:hello@studioteepee.co.uk">hello@studioteepee.co.uk</a>
+            </dd>
 
-        <h1>contact</h1>
+            <dt>
+              Phone
+            </dt>
+            <dd>
+              01353 611721
+            </dd>
 
-        <div className="description">
-          <div className="col">
-            <p>
-              Creating, extending, or remodelling homes is always exciting.
-              We tailor our design approach to suit each clients priorities,
-              whether they are space, light, connection to the outdoors,
-              sustainability, materiality, simply ensuring the full potential
-              of each home is achieved, or normally – all of the above!
-            </p>
-          </div>
+            <dt>
+              Mobile
+            </dt>
+            <dd>
+              07534323431
+            </dd>
 
-          <div className="col">
-            <p>
-              From modest alterations, to creating new homes from scratch,
-              <b className="brand">studio teepee</b> can provide the full
-              range of architectural services:-
-            </p>
-            <ul>
-              <li>
-                Providing designs to remodel, extend, or create new homes
-              </li>
-              <li>
-                Preparing planning and building regulations applications
-              </li>
-              <li>
-                Preparing tender information and appointing contractors
-              </li>
-              <li>
-                Preparing construction information
-              </li>
-              <li>
-                Contract administration
-              </li>
-            </ul>
-          </div>
+            <dt>
+              Address
+            </dt>
+            <dd>
+              4 Willow Walk, Ely<br />
+              CB7 4AT
+            </dd>
 
-          <div className="col">
-            <p>
-              Our past projects have been diverse, with each providing unique
-              challenges and interest:-
-            </p>
-            <ul>
-              <li>
-                Extensions and alterations to listed buildings
-              </li>
-              <li>
-                Extensions and new homes within Conservation Areas
-              </li>
-              <li>
-                New homes in the open countryside
-              </li>
-              <li>
-                Crafted extensions in tight urban settings
-              </li>
-              <li>
-                The renovation and extension of post-war prefabricated
-                concrete frame buildings
-              </li>
-              <li>
-                New build straw bale construction
-              </li>
-              <li>
-                Converting shops to become new homes
-              </li>
-            </ul>
-          </div>
+            <dt>
+              Follow me
+            </dt>
+            <dd>
+              <SocialLinks />
+            </dd>
+          </dl>
+
+          <p className="privacy">
+            I respect your privacy.
+            Your details will never be shared with anyone else, ever.
+          </p>
         </div>
+
+        <ContactForm page="contact page" />
       </div>
     </main>
   );
