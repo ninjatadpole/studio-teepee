7d9e9027a273a021551d70300a73338d35ded87d
Andy Quin <andy@sharpfrog.com>
2017-11-12 12:37:31 +0000

    remove service workers if I accidentally installed them -- remove this before live

--- teepee/src/index.js
+++ teepee/src/index.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+//registerServiceWorker();
+window.__testCleanup = () => {
+  const unregisterSW = () => {
+    return navigator.serviceWorker.getRegistrations()
+    .then((registrations) => {
+      const unregisterPromise = registrations.map((registration) => {
+        return registration.unregister();
+      });
+      return Promise.all(unregisterPromise);
+    });
+  };
+
+  const clearCaches = () => {
+    return window.caches.keys()
+    .then((cacheNames) => {
+      return Promise.all(cacheNames.map((cacheName) => {
+        return window.caches.delete(cacheName);
+      }));
+    });
+  };
+
+  if (navigator.serviceWorker) {
+    return Promise.all([
+      unregisterSW(),
+      clearCaches(),
+    ]);
+  }
+};
+
+window.__testCleanup();
