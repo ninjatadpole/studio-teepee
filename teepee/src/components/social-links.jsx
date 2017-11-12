ec822e630ee8b8b030983ef74791bad78f19065c
Andy Quin <andy@sharpfrog.com>
2017-11-12 14:42:03 +0000

    Open social links in a new window

--- teepee/src/components/social-links.jsx
+++ teepee/src/components/social-links.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
 
 function SocialLinks (props) {
+  const openInNewTab = (e) => {
+    const link = e.target;
+    e.preventDefault();
+    window.open(link.getAttribute('href'), link.getAttribute('title'));
+  }
+
   return (
     <ul className="social-links">
       <li className="linkedin">
-        <a href="https://www.linkedin.com/company/18096317/" className="icon-logo-linkedin">
+        <a
+          href="https://www.linkedin.com/in/matt-plummer-048914a0/"
+          className="icon-logo-linkedin"
+          onClick={ openInNewTab }
+        >
           <span className="title">LinkedIn</span>
         </a>
       </li>
       <li className="facebook">
-        <a href="https://www.facebook.com/studioteepee.hi" className="icon-logo-facebook">
+        <a
+          href="https://www.facebook.com/studioteepee.hi"
+          className="icon-logo-facebook"
+          onClick={ openInNewTab }
+        >
           <span className="title">Facebook</span>
         </a>
       </li>
       <li className="twitter">
-        <a href="https://twitter.com/studioteepee_hi" className="icon-logo-twitter">
+        <a
+          href="https://twitter.com/studioteepee_hi"
+          className="icon-logo-twitter"
+          onClick={ openInNewTab }
+        >
           <span className="title">Twitter</span>
         </a>
       </li>
