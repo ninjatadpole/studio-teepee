dc3e0ec0d767bdf26814d64bd0584670a82be318
Andy Quin <andy@sharpfrog.com>
2017-11-18 17:27:28 +0000

    include both hero and sub images for project details page

---
+++ teepee/src/components/projects/crescent-extension.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const ProjectDescription = function(props) {
+  return (
+    <div>
+      <p>
+        This high-specification extension creates an open-plan dining and kitchen
+        area, which opens directly onto the enclosed garden. The form and proportions
+        of the building and the expansive glazing are reflections of the distinct
+        geometry of the original Grade II Listed Georgian townhouse, itself built in
+        approximately 1820.
+      </p>
+      <p>
+        The sliding glass uses the narrowest framing system available at the time, yet
+        achieves a 2.6x2.8m sliding glass unit, with a returning panel which slides
+        into a concealed pocket. In summer, the room has a 'pavilion' character opening
+        onto the garden, and with a large roof light installed at the junction with the
+        main house, natural light floods the new kitchen.
+      </p>
+      <p>
+        During cooler months, a concealed curtain track allows heavy, thermally-lined
+        curtains to be installed to retain heat. This changes the quality of the spaces
+        to become softened and warm. Thermal performances for the floor, walls and roof
+        are also significantly enhanced.
+      </p>
+      <p className="ps">
+        The scheme was given a Commendation in the Spirit of Ingenuity awards, RIBA
+        East.<br />
+        The project was completed while Matt was with CSA Design Studio in Norfolk.
+        Images courtesy of CSA Design Studio
+      </p>
+    </div>
+  );
+}
+
+export default ProjectDescription;
--- teepee/src/pages/project-details.jsx
+++ teepee/src/pages/project-details.jsx
@@ -13,13 +13,39 @@ const ProjectDetails = function(props) {
   }, null);
 
   if (projectMatch) {
+    const heroImg = require(`../assets/images/projects/${projectId}/hero.jpg`);
+    const Description = require(`../components/projects/${projectId}`).default;
+
     return (
       <main className="page-project-details">
         <h1>{ projectMatch.type }</h1>
         <h2>{ projectMatch.location }</h2>
 
         <div className="content">
-          Some images and stuff
+          {
+            heroImg &&
+            <div className="hero">
+              <img src={ heroImg } alt="" />
+            </div>
+          }
+          <div className="description">
+            <Description />
+          </div>
+          {
+            projectMatch.images &&
+            <div className="images">
+              {
+                projectMatch.images.reduce((aggregate, image, index) => {
+                  const imgSrc = require(`../assets/images/projects/${projectId}/${image}`);
+                  return [
+                    ...aggregate,
+                    <img src={ imgSrc } alt="" key={ `image-${index}` } />
+                  ];
+                }, [])
+              }
+            </div>
+          }
+
         </div>
       </main>
     );
