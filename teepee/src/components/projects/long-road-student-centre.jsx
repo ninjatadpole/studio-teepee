f4b9f5d570d17e0632657f6cb52acb95838748a0
Andy Quin <andy@sharpfrog.com>
2017-12-05 20:33:40 +0000

    Add long road student centre project

---
+++ teepee/src/components/projects/long-road-student-centre.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ProjectDescription = function(props) {
+  return (
+    <div>
+      <p>
+        A new Student Centre has been created within the courtyards of Long Road
+        Sixth Form College by remodelling a redundant, isolated block.
+      </p>
+      <p>
+        The building not only allows students a place to rest and relax between
+        lessons, the open-plan layout is also used for student functions and
+        open-days.
+      </p>
+      <p className="ps">
+        This project was completed while Matt was with RH Partnership Architects
+        in Cambridge. Images courtesy of RH Partnership.
+      </p>
+    </div>
+  );
+}
+
+export default ProjectDescription;
