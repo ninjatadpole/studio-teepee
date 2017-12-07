498ed2c8910e04782657f763f7af514c2ff16eb2
Andy Quin <andy@sharpfrog.com>
2017-12-07 22:09:26 +0000

    refactor routing to include automatic scroll to top behaviour

--- teepee/src/App.js
+++ teepee/src/App.js
@@ -1,48 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import HomePage from './pages/home';
-import ApproachPage from './pages/approach';
-import ContactPage from './pages/contact';
-import ProjectListPage from './pages/project-list';
-import ProjectDetailsPage from './pages/project-details';
-
-import Footer from './components/footer';
-import Header from './components/header';
+import RouteContents from './components/route-contents';
+import ScrollToTop from './components/scroll-to-top';
 
 import './styles/teepee.css';
 
 const App = () => (
   <Router>
-    <Contents />
+    <ScrollToTop>
+      <RouteContents />
+    </ScrollToTop>
   </Router>
 )
-
-const Contents = (props) => {
-  return (
-    <div className="teepee container-fluid">
-      <Header />
-
-      <Switch>
-        <TrackedRoute exact path="/" component={ HomePage }/>
-        <TrackedRoute exact path="/projects" component={ ProjectListPage }/>
-        <TrackedRoute path="/projects/:projectId" component={ ProjectDetailsPage }/>
-        <TrackedRoute path="/approach" component={ ApproachPage }/>
-        <TrackedRoute path="/contact" component={ ContactPage }/>
-        <TrackedRoute component={ HomePage }/>
-      </Switch>
-
-      <Footer />
-    </div>
-  );
-}
-
-const TrackedRoute = (props) => {
-  window.ga('send', 'pageview', props.location.pathname);
-
-  return (
-    <Route { ...props } />
-  );
-}
 
 export default App;
---
+++ teepee/src/components/route-contents.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import HomePage from '../pages/home';
+import ApproachPage from '../pages/approach';
+import ContactPage from '../pages/contact';
+import ProjectListPage from '../pages/project-list';
+import ProjectDetailsPage from '../pages/project-details';
+
+import Footer from './footer';
+import Header from './header';
+
+const RouteContents = (props) => {
+  return (
+    <div className="teepee container-fluid">
+      <Header />
+
+      <Switch>
+        <TrackedRoute exact path="/" component={ HomePage }/>
+        <TrackedRoute exact path="/projects" component={ ProjectListPage }/>
+        <TrackedRoute path="/projects/:projectId" component={ ProjectDetailsPage }/>
+        <TrackedRoute path="/approach" component={ ApproachPage }/>
+        <TrackedRoute path="/contact" component={ ContactPage }/>
+        <TrackedRoute component={ HomePage }/>
+      </Switch>
+
+      <Footer />
+    </div>
+  );
+}
+
+const TrackedRoute = (props) => {
+  window.ga('send', 'pageview', props.location.pathname);
+
+  return (
+    <Route { ...props } />
+  );
+}
+
+export default RouteContents;
